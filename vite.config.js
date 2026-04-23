ca9073d7a453cd31a371611b3000054a8b32a737
Esteban Timoteo <nabirp_nabetse@hotmail.com>
2026-04-23 17:56:28 -0600

    nombrar proyecto en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/nombre-del-repo/', // Debe ser el nombre exacto de tu repo en GitHub
-})
+  base: "/gathsession/", // Debe ser el nombre exacto de tu repo en GitHub
+});
